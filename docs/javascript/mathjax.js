f9e8e3267c61b57a2d251f54e403202c496a6d1f
moputradopol <34512105+moputradopol@users.noreply.github.com>
2026-08-22 02:26:47 -0400

    Update mathjax.js

--- docs/javascript/mathjax.js
+++ docs/javascript/mathjax.js
@@ -1,8 +1,19 @@
 window.MathJax = {
   tex: {
-    inlineMath: [["$", "$"], ["\\(", "\\)"]],
-    displayMath: [["$$", "$$"], ["\\[", "\\]"]],
-    processEscapes: true
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
   },
-  svg: { fontCache: "global" }
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
 };
+
+document$.subscribe(() => { 
+  MathJax.startup.output.clearCache()
+  MathJax.typesetClear()
+  MathJax.texReset()
+  MathJax.typesetPromise()
+})
